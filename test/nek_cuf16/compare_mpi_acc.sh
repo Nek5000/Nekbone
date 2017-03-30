d23856d7b85739c46a2fd1d569317965faa57626
Ron Rahaman <rahaman@cs.uchicago.edu>
2017-03-30 16:21:43 +0000

    .gitignore ignores cobalt job log/output/error files

--- test/nek_cuf16/compare_mpi_acc.sh
+++ test/nek_cuf16/compare_mpi_acc.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
- make -f makefile.mpi.jlse  clean
+ make -f makefile.mpi.jlse clean
  make -f makefile.acc.jlse clean
 
- make -f makefile.mpi.jlse  > compile.mpi.stdout  2> compile.mpi.stderr  || cat compile.mpi.stderr
- make -f makefile.acc.jlse > compile.acc.stdout 2> compile.acc.stderr || cat compile.acc.stderr
+ make -f makefile.mpi.jlse > compile.mpi.output 2> compile.mpi.error || cat compile.mpi.error
+ make -f makefile.acc.jlse > compile.acc.output 2> compile.acc.error || cat compile.acc.error
 
- mpiexec -n 1 ./nekbone_mpi data  > run.mpi.stdout  2> run.mpi.stderr  || cat run.mpi.stderr
- mpiexec -n 1 ./nekbone_acc data > run.acc.stdout 2> run.acc.stderr || cat run.acc.stderr
+ mpiexec -n 1 ./nekbone_mpi data > run.mpi.output 2> run.mpi.error || cat run.mpi.error
+ mpiexec -n 1 ./nekbone_acc data > run.acc.output 2> run.acc.error || cat run.acc.error
 
- vimdiff run.mpi.stdout run.acc.stdout
+ vimdiff run.mpi.output run.acc.output
 
--- test/nek_cuf16/compare_mpi_cuda.sh
+++ test/nek_cuf16/compare_mpi_cuda.sh
@@ -3,11 +3,11 @@
  make -f makefile.mpi.jlse  clean
  make -f makefile.cuda.jlse clean
 
- make -f makefile.mpi.jlse  > compile.mpi.stdout  2> compile.mpi.stderr  || cat compile.mpi.stderr
- make -f makefile.cuda.jlse > compile.cuda.stdout 2> compile.cuda.stderr || cat compile.cuda.stderr
+ make -f makefile.mpi.jlse  > compile.mpi.output  2> compile.mpi.error  || cat compile.mpi.error
+ make -f makefile.cuda.jlse > compile.cuda.output 2> compile.cuda.error || cat compile.cuda.error
 
- mpiexec -n 1 ./nekbone_mpi data  > run.mpi.stdout  2> run.mpi.stderr  || cat run.mpi.stderr
- mpiexec -n 1 ./nekbone_cuda data > run.cuda.stdout 2> run.cuda.stderr || cat run.cuda.stderr
+ mpiexec -n 1 ./nekbone_mpi data  > run.mpi.output  2> run.mpi.error  || cat run.mpi.error
+ mpiexec -n 1 ./nekbone_cuda data > run.cuda.output 2> run.cuda.error || cat run.cuda.error
 
- vimdiff run.mpi.stdout run.cuda.stdout
+ vimdiff run.mpi.output run.cuda.output
 
